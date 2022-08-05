4f508e48aa1ae0d534d945dd8f9e25b070441c63
Nurbolsun <kg.nurbo.97@mail.ru>
2022-08-05 20:01:34 +0600

    lp

--- lesson_9/src/Main.java
+++ lesson_9/src/Main.java
@@ -1,6 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        char operator;
+        double num1, num2;
+        System.out.println("Введите первое число:");
+        num1 = scan.nextDouble();
+        System.out.println("Выберите:");
+        operator = scan.next().charAt(0);
+        System.out.println("Введите второе число:");
+        num2 = scan.nextDouble();
+
+        switch (operator){
+
+        }
+        System.out.println();
     }
 }
