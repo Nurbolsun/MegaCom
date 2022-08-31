c6cc693e3b6cfcd560197f5bd3fb3b2ff4191bce
Nurbolsun <kg.nurbo.97@mail.ru>
2022-08-31 20:04:18 +0600

    laptop

--- enum/src/Main.java
+++ enum/src/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
         for (Elements iteam:Elements.values() ) {
             System.out.println(iteam.getName() + " Номер элемента в таблице: " + iteam.getNum());
-        }
+        } 
     }
 }
