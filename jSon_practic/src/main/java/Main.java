01c02ab6e2038c27d38537c80f26b7d21337a789
Nurbolsun <kg.nurbo.97@mail.ru>
2022-11-29 21:07:28 +0600

    lp

---
+++ jSon_practic/src/main/java/Main.java
@@ -0,0 +1,41 @@
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import microservices.OkHttoConnection;
+import models.Employee;
+import models.Hobby;
+import service.UserService;
+import service.impl.UserServiceImpl;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static jdk.nashorn.internal.parser.TokenType.XML;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        UserService userService = new UserServiceImpl();
+//        System.out.println(userService);
+
+        Employee employee = new Employee();
+
+        OkHttoConnection connection = new OkHttoConnection();
+        String response = connection.getJson();
+
+        JSONObject jsonObject = XML.toJSONObject(response);
+        Gson gson = new Gson();
+
+        Employee[] employees = gson.fromJson(response, Employee[].class);
+        Arrays.stream(employees);
+
+
+
+
+
+        System.out.println("F1 " + employees);
+        System.out.println("F2 "+employeeList2);
+
+
+    }
+}
