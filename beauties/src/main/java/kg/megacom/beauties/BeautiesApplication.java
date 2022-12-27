101689722ee2c44bb7d4e3f0bc49e3149e8166ba
Nurbolsun <kg.nurbo.97@mail.ru>
2022-12-27 12:45:42 +0600

    beauty

---
+++ beauties/src/main/java/kg/megacom/beauties/BeautiesApplication.java
@@ -0,0 +1,13 @@
+package kg.megacom.beauties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeautiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeautiesApplication.class, args);
+	}
+
+}
